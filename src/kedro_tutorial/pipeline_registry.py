535a7b379971358ab22ec914725ac6ae25fa4429
Edmund Miller <edmund.a.miller@protonmail.com>
2021-11-28 16:48:03 -0600

    feat: Add data_processing pipeline

--- src/kedro_tutorial/pipeline_registry.py
+++ src/kedro_tutorial/pipeline_registry.py
@@ -31,6 +31,8 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline
 
+from kedro_tutorial.pipelines import data_processing as dp
+
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -38,4 +40,9 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": Pipeline([])}
+    data_processing_pipeline = dp.create_pipeline()
+
+    return {
+        "__default__": data_processing_pipeline,
+        "dp": data_processing_pipeline,
+    }
