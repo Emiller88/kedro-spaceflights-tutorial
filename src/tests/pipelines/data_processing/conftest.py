12f080a7c97641b00a608b38f2950c2f5f7d6176
Edmund Miller <edmund.a.miller@protonmail.com>
2021-11-28 20:54:38 -0600

    test(processing): Add test for preprocess_companies

--- src/tests/pipelines/data_processing/conftest.py
+++ src/tests/pipelines/data_processing/conftest.py
@@ -1,1 +1,16 @@
-#!/usr/bin/env python3
+import pandas as pd
+import pytest
+
+
+@pytest.fixture(scope="module")
+def basic_data():
+    return pd.DataFrame(
+        {
+            "id": [
+                35029,
+                30292,
+            ],
+            "company_rating": ["100%", "67%"],
+            "iata_approved": ["f", "f"],
+        }
+    )
--- src/tests/pipelines/data_processing/test_nodes.py
+++ src/tests/pipelines/data_processing/test_nodes.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from kedro_tutorial.pipelines.data_processing.nodes import (
     _is_true,
     create_model_input_table,
@@ -5,7 +6,27 @@ from kedro_tutorial.pipelines.data_processing.nodes import (
     preprocess_shuttles,
 )
 
+
 def test_is_true():
     assert _is_true("t") == True
     assert _is_true("f") == False
     assert _is_true("a") == False
+
+
+def test_preprocess_companies(basic_data):
+
+    output = preprocess_companies(basic_data)
+
+    print(output)
+    assert output.equals(
+        pd.DataFrame(
+            {
+                "id": [
+                    35029,
+                    30292,
+                ],
+                "company_rating": [1, 0.67],
+                "iata_approved": [False, False],
+            }
+        )
+    )
